1cc3f5ff7dea897dec4a1fcdf184c1911563240d
Valentin Plugaru <valentin.plugaru@uni.lu>
2016-02-16 14:21:59 +0100

    Remove legacy code.
    Signed-off-by: Valentin Plugaru <valentin.plugaru@uni.lu>

--- manifests/node/debian.pp
+++ manifests/node/debian.pp
@@ -1,16 +1,16 @@
-# File::      <tt>node/redhat.pp</tt>
+# File::      <tt>node/debian.pp</tt>
 # Author::    S. Varrette, H. Cartiaux, V. Plugaru, S. Diehl aka. UL HPC Management Team (hpc-sysadmins@uni.lu)
 # Copyright:: Copyright (c) 2016 S. Varrette, H. Cartiaux, V. Plugaru, S. Diehl aka. UL HPC Management Team
 # License::   Gpl-3.0
 #
 # ------------------------------------------------------------------------------
-# = Class: ganglia::node::redhat
+# = Class: ganglia::node::debian
 #
-# Specialization class for Redhat systems
-class ganglia::node::redhat inherits ganglia::node::common {
+# Specialization class for Debian systems
+class ganglia::node::debian inherits ganglia::node::common {
 
     File[$ganglia::params::configfile] {
-        content => template('ganglia/redhat.gmond.conf.erb'),
+        content => template('ganglia/debian.gmond.conf.erb'),
     }
 
 }
--- manifests/node/redhat.pp
+++ manifests/node/redhat.pp
@@ -1,10 +1,16 @@
-# File::      <tt>common/redhat.pp</tt>
+# File::      <tt>node/redhat.pp</tt>
 # Author::    S. Varrette, H. Cartiaux, V. Plugaru, S. Diehl aka. UL HPC Management Team (hpc-sysadmins@uni.lu)
 # Copyright:: Copyright (c) 2016 S. Varrette, H. Cartiaux, V. Plugaru, S. Diehl aka. UL HPC Management Team
 # License::   Gpl-3.0
 #
 # ------------------------------------------------------------------------------
-# = Class: ganglia::common::redhat
+# = Class: ganglia::node::redhat
 #
 # Specialization class for Redhat systems
-class ganglia::common::redhat inherits ganglia::common { }
+class ganglia::node::redhat inherits ganglia::node::common {
+
+    File[$ganglia::params::configfile] {
+        content => template('ganglia/redhat.gmond.conf.erb'),
+    }
+
+}
