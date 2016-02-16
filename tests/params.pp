f8ffad7726f42167aaa29127a43b01283cb4633e
Valentin Plugaru <valentin.plugaru@uni.lu>
2016-02-16 12:32:38 +0100

    update content of 'params.pp'
    Signed-off-by: Valentin Plugaru <valentin.plugaru@uni.lu>

--- tests/params.pp
+++ tests/params.pp
@@ -15,12 +15,25 @@
 
 include 'ganglia::params'
 
-$names = ['ensure', 'protocol', 'port', 'packagename']
+$names = ["ensure", "packagename", "serverpackagename", "servicename", "serverservicename", "gmond_status_command", "ibgit", "ibtarget", "ibmakedep", "configfile", "serverconfigfile", "webconfigfile", "apacheconfigfile", "apachetarget", "configfile_mode", "configfile_owner", "configfile_group"]
 
 notice("ganglia::params::ensure = ${ganglia::params::ensure}")
-notice("ganglia::params::protocol = ${ganglia::params::protocol}")
-notice("ganglia::params::port = ${ganglia::params::port}")
 notice("ganglia::params::packagename = ${ganglia::params::packagename}")
+notice("ganglia::params::serverpackagename = ${ganglia::params::serverpackagename}")
+notice("ganglia::params::servicename = ${ganglia::params::servicename}")
+notice("ganglia::params::serverservicename = ${ganglia::params::serverservicename}")
+notice("ganglia::params::gmond_status_command = ${ganglia::params::gmond_status_command}")
+notice("ganglia::params::ibgit = ${ganglia::params::ibgit}")
+notice("ganglia::params::ibtarget = ${ganglia::params::ibtarget}")
+notice("ganglia::params::ibmakedep = ${ganglia::params::ibmakedep}")
+notice("ganglia::params::configfile = ${ganglia::params::configfile}")
+notice("ganglia::params::serverconfigfile = ${ganglia::params::serverconfigfile}")
+notice("ganglia::params::webconfigfile = ${ganglia::params::webconfigfile}")
+notice("ganglia::params::apacheconfigfile = ${ganglia::params::apacheconfigfile}")
+notice("ganglia::params::apachetarget = ${ganglia::params::apachetarget}")
+notice("ganglia::params::configfile_mode = ${ganglia::params::configfile_mode}")
+notice("ganglia::params::configfile_owner = ${ganglia::params::configfile_owner}")
+notice("ganglia::params::configfile_group = ${ganglia::params::configfile_group}")
 
 #each($names) |$v| {
 #    $var = "ganglia::params::${v}"
